49c4f5fc3c88be1f4f6401bb385114ed0874a426
joshieadalid <josueadalidjb@gmail.com>
2024-12-19 07:39:35 -0600

    TTS terminado 2

--- blueprints/modes_bp.py
+++ blueprints/modes_bp.py
@@ -7,3 +7,7 @@ modes_bp = Blueprint('modes', __name__)
 @modes_bp.route('/meeting')
 def mode_meeting():
     return render_template('modes/mode_meeting.html')
+
+@modes_bp.route('/minutes')
+def mode_minute():
+    return render_template('modes/minutes.html')
